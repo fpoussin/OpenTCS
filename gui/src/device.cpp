8c2dbb422c4ef8c7177f4f23da1a6777ea25388a
mobyfab <mobyfab@free.fr>
2013-09-24 19:27:55 +0200

    Minor fixes.

--- gui/src/device.cpp
+++ gui/src/device.cpp
@@ -5,6 +5,7 @@ device::device(QObject *parent) :
 {
 
     iNumDevs = 0;
+    pcBufLD[0] = cBufLD;
 }
 
 bool device::connect(void)
@@ -13,7 +14,7 @@ bool device::connect(void)
     this->ftStatus = FT_ListDevices(pcBufLD, &iNumDevs, FT_LIST_ALL | FT_OPEN_BY_SERIAL_NUMBER);
 
     if(ftStatus != FT_OK) {
-        qDebug("Error: FT_ListDevices(%d)\n", (int)ftStatus);
+        qWarning("Error: FT_ListDevices(%d)\n", (int)ftStatus);
         return true;
     }
 
@@ -23,16 +24,16 @@ bool device::connect(void)
             use lsmod to check this and rmmod ftdi_sio to remove
             also rmmod usbserial
         */
-        qDebug("Error FT_OpenEx(%d), device %d\n", (int)ftStatus, 0);
-        qDebug("Use lsmod to check if ftdi_sio (and usbserial) are present.\n");
-        qDebug("If so, unload them using rmmod, as they conflict with ftd2xx.\n");
+        qWarning("Error FT_OpenEx(%d), device %d\n", (int)ftStatus, 0);
+        qWarning("Use lsmod to check if ftdi_sio (and usbserial) are present.\n");
+        qWarning("If so, unload them using rmmod, as they conflict with ftd2xx.\n");
         return 1;
     }
 
     FT_SetTimeouts(ftHandle, 3000, 3000);	// 3 second read timeout
 
     if((ftStatus = FT_SetBaudRate(ftHandle, 115200)) != FT_OK) {
-        qDebug("Error FT_SetBaudRate(%d), cBufLD[i] = %s\n", (int)ftStatus, cBufLD);
+        qWarning("Error FT_SetBaudRate(%d), cBufLD[i] = %s\n", (int)ftStatus, cBufLD);
         return 1;
     }
 
@@ -51,7 +52,7 @@ bool device::write(char * buf, unsigned int len)
     unsigned int dwBytesWritten;
     ftStatus = FT_Write(ftHandle, buf, len, &dwBytesWritten);
     if (ftStatus != FT_OK) {
-        qDebug("Error FT_Write(%d)\n", (int)ftStatus);
+        qWarning("Error FT_Write(%d)\n", (int)ftStatus);
         return true;
     }
     return false;
@@ -62,7 +63,7 @@ bool device::read(char * buf, unsigned int len)
     unsigned int dwBytesRead;
     ftStatus = FT_Read(ftHandle, buf, len, &dwBytesRead);
     if (ftStatus != FT_OK) {
-        qDebug("Error FT_Read(%d)\n", (int)ftStatus);
+        qWarning("Error FT_Read(%d)\n", (int)ftStatus);
         return true;
     }
     return false;
@@ -75,7 +76,7 @@ bool device::readAll(char *buf, unsigned int max_len)
     ftStatus = FT_GetQueueStatus(ftHandle, &len);
 
     if(ftStatus != FT_OK) {
-        qDebug("Error FT_GetQueueStatus(%d)\n", (int)ftStatus);
+        qWarning("Error FT_GetQueueStatus(%d)\n", (int)ftStatus);
         return 1;
     }
 
@@ -108,7 +109,7 @@ bool device::resetBootloader()
 bool device::setCBUS(int mask)
 {
     if(FT_SetBitMode(ftHandle, mask, 0x20) != FT_OK) {
-        qDebug("Failed to set CBUS bit mask: %d\n", mask);
+        qWarning("Failed to set CBUS bit mask: %d\n", mask);
         return 1;
     }
 
